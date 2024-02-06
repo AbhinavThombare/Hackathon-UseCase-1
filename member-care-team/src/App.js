4aaec14d8086dfcd0c88c6569bf07e25a4f96a06
Hritvik-55 <agarwal.hritvik18@gmail.com>
2024-02-06 20:44:52 +0530

    removed comments in App.js

--- member-care-team/src/App.js
+++ member-care-team/src/App.js
@@ -8,44 +8,6 @@ import FollowUp from './components/FollowUp/followUp';
 import Notification from './components/Notification/notification';
 import Coupon from './components/coupon/coupon';
 import Landing from './components/Landing/Landing';
-// const router = createBrowserRouter([
-//   {
-//     path:'/',
-//     element:<Details/>,
-//     id:'root'
-//   },
-//   {
-//     path:'detials',
-//     element: <Details/>
-//   },
-//   {
-//     path:'medicine',
-//     element: <MedicineDetials/>
-//   },
-//   {
-//     path:'followup',
-//     element: <FollowUp/>
-//   },
-//   {
-//     path:'notification',
-//     element: <Notification/>
-//   },
-//   {
-//     path:'coupon',
-//     element: <Coupon/>
-//   }
-//   ,
-//   {
-//     path:'landing',
-//     element: <Landing/>
-//   }
-// ]);
-
-// function App() {
-//   return (
-//     <RouterProvider router={router}></RouterProvider>
-//   );
-// }
 
 import React from 'react'
 import Header from './components/Header/header';
