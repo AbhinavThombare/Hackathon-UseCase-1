fb029f4da6bc5571dd9b997a559bc58061a50ee0
Hritvik-55 <agarwal.hritvik18@gmail.com>
2024-02-06 16:52:52 +0530

    piechart Component

--- member-care-team/src/components/Details/details.js
+++ member-care-team/src/components/Details/details.js
@@ -12,7 +12,9 @@ import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import PatientData from '../../patient_data.json'
 
+
 import './details.css'
+import PieChartWithCustomizedLabel from './Piechart/piechart';
 
 const Details = () => {
 
@@ -148,11 +150,7 @@ const Details = () => {
                     </div>
                 </div>
 
-                <div className='extra-div'>
-                    <div className='extra-text-area-1'>
-                        <div className='section-1'>
-
-                        </div>
+                
                     </div>
 
                     <div className='extra-text-area-2'>
@@ -160,8 +158,9 @@ const Details = () => {
 
                         </div>
                     </div>
-                </div>
-            </div>
+                
+                
+            
         </>
     )
 }
